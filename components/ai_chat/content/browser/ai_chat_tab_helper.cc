5717e71dbd60b7a460a36bb8d259fd53184a2c8b
Anthony Tseng <darkdh@gmail.com>
2024-03-07 13:47:00 -0800

    Impose page limit and content limit for preview page processing

--- components/ai_chat/content/browser/ai_chat_tab_helper.cc
+++ components/ai_chat/content/browser/ai_chat_tab_helper.cc
@@ -94,6 +94,7 @@ void AIChatTabHelper::OnPDFA11yInfoLoaded() {
   is_pdf_a11y_info_loaded_ = true;
   if (pending_get_page_content_callback_) {
     FetchPageContent(web_contents(), "", std::nullopt,
+                     GetCurrentModel().max_page_content_length,
                      std::move(pending_get_page_content_callback_));
   }
   pdf_load_observer_.reset();
@@ -106,6 +107,7 @@ void AIChatTabHelper::OnPreviewReady(
     const std::optional<std::vector<SkBitmap>>& bitmaps) {
   if (pending_get_page_content_callback_) {
     FetchPageContent(web_contents(), "", bitmaps,
+                     GetCurrentModel().max_page_content_length,
                      std::move(pending_get_page_content_callback_));
   }
 }
